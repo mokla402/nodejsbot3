947e0418fbfafa6e2d0a209a74c54f9ff69319f8
mokla402 <happysehoon@naver.com>
2020-11-11 17:19:24 +0900

    수정

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.on('message', (message) => {
   if(message.author.bot) return;
 
   if(message.content == '비니야') {
-    return message.reply('웅');
+    return message.reply('왜 불러');
   }
 
   if(message.content == 'embed') {
